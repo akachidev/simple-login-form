ead0c9c2dac574edf0c1d401fb9987348f84dd95
Tutorials-Git <codewithprince.com@gmail.com>
2022-08-14 13:41:27 +0100

    pushed to github

---
+++ chichi.js
@@ -0,0 +1,19 @@
+
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "user" && password === "web_dev") {
+        alert("You have successfully logged in.");
+        location.reload();
+
+        setTimeout(() => login-error-msg-holder.remove(), 3.000);
+    } else {
+     loginErrorMsg.style.opacity = 1;
+    }
+})
